1f9187934b54edde9c9ed3761e68b4e8c1ecc987
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-08 13:07:51 +0700

    Add map_test

---
+++ map_test.go
@@ -0,0 +1,61 @@
+package go_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestMap(t *testing.T) {
+	var data sync.Map
+	var addToMap = func(value int) {
+		data.Store(value, value)
+	}
+
+	for i := 0; i < 100; i++ {
+		go addToMap(i)
+	}
+
+	data.Range(func(key, value interface{}) bool {
+		fmt.Println(key, ":", value)
+		return true
+	})
+
+}
+
+func TestSimpleMap(t *testing.T) {
+	data := sync.Map{}
+	data.Store("key", "value")
+	data.Store("key2", "value2")
+	data.Range(func(key, value interface{}) bool {
+		fmt.Println(key, ":", value)
+		return true
+	})
+}
+
+func TestAnotherMap(t *testing.T) {
+	wg := sync.WaitGroup{}
+	m := sync.Map{}
+
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(j int) {
+			m.Store(j, fmt.Sprintf("test %v", j))
+			// fmt.Println(m.Load(j))
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Done.")
+
+	for i := 0; i < 5; i++ {
+		t, _ := m.Load(i)
+		fmt.Println("for loop: ", t)
+	}
+
+	m.Range(func(k, v interface{}) bool {
+		fmt.Println(v)
+		return true
+	})
+}
